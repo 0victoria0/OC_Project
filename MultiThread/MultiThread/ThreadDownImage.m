3524a7aeded180c70f1c48567fcd84fa05f466e1
victoria <1491693608@qq.com>
2016-10-10 17:58:01 +0800

    采用队列的形式进行多任务请求后更新🈶UI

--- MultiThread/MultiThread/ThreadDownImage.m
+++ MultiThread/MultiThread/ThreadDownImage.m
@@ -29,7 +29,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self threadDownImage];
+//    [self threadDownImage];
+    [self threadGroup];
 }
 
 - (void)startLoading{
@@ -111,6 +112,42 @@
     }else if (self.count == -1){
         NSLog(@"下载请求失败");
     }
+}
+
+#pragma mark -
+#pragma mark -  采用队列组的形式实现
+- (void)threadGroup{
+    [self startLoading];
+    dispatch_group_t group =  dispatch_group_create();
+    dispatch_group_async(group, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // 执行1个耗时的异步操作
+        NSURL *url = [NSURL URLWithString:@"http://f.hiphotos.baidu.com/image/w%3D2048/sign=91c1063e1f950a7b753549c43ee963d9/f31fbe096b63f624b6a9640b8544ebf81b4ca3c6.jpg"];
+        NSData *data = [[NSData alloc] initWithContentsOfURL:url];
+        image1 = [UIImage imageWithData:data];
+    });
+    dispatch_group_async(group, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // 执行1个耗时的异步操作
+        [NSThread sleepForTimeInterval:5];
+        //下载图片2
+        NSURL *url2= [NSURL URLWithString:@"http://h.hiphotos.baidu.com/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=f47fd63ca41ea8d39e2f7c56f6635b2b/1e30e924b899a9018b8d3ab11f950a7b0308f5f9.jpg"];
+        NSData *data2 = [[NSData alloc] initWithContentsOfURL:url2];
+        image2 = [UIImage imageWithData:data2];
+    });
+    
+    dispatch_group_async(group, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // 执行1个耗时的异步操作
+        NSURL *url = [NSURL URLWithString:@"http://f.hiphotos.baidu.com/image/w%3D2048/sign=91c1063e1f950a7b753549c43ee963d9/f31fbe096b63f624b6a9640b8544ebf81b4ca3c6.jpg"];
+        NSData *data = [[NSData alloc] initWithContentsOfURL:url];
+        image3 = [UIImage imageWithData:data];
+    });
+    
+    dispatch_group_notify(group, dispatch_get_main_queue(), ^{
+        // 等前面的异步操作都执行完毕后，回到主线程...
+        [self stopLoading];
+        self.imageView1.image = image1;
+        self.imageView2.image = image2;
+        self.imageView3.image = image3;
+    });
 }
 
 - (void)didReceiveMemoryWarning {
